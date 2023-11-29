1ad3e27ca7596172a8302fabf6947c761c4710e2
Jose Arroyave <poer0601@hotmail.com>
2023-11-29 17:21:08 -0500

    terminar lógica de doc entrega

--- app/Http/Controllers/FpEntregasController.php
+++ app/Http/Controllers/FpEntregasController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\FpDocumentosEntregasModel;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use App\Models\FpProductosPorPedidosModel;
+use App\Models\FpDocumentosEntregasModel;
+use Barryvdh\DomPDF\Facade\Pdf as PDF;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Log;
+use App\Models\FpGuiasEnvioModel;
 
 class FpEntregasController extends Controller
 {
@@ -36,6 +38,58 @@ class FpEntregasController extends Controller
       }
 
       return FpGuiasEnvioController::generateGuiasTransporte();
+    } catch (\Throwable $th) {
+      return response()->json(["Error" => $th->getMessage(), "Línea" => $th->getLine(), "Archivo" => __FILE__]);
+    }
+  }
+
+  public function getDocumentosPerGuia($id_guia, $notApi = false)
+  {
+    try {
+
+      $idLineaProductoPerGuia = FpGuiasEnvioModel::where("id_guia", $id_guia)->pluck("id_linea_producto");
+      $idLineaProductoPerGuia = FpProductosPorPedidosModel::join("fp_productos", "fp_productos_x_pedido.SKU", "fp_productos.SKU")
+        ->whereIn("fp_productos_x_pedido.id", $idLineaProductoPerGuia)->get();
+
+      if ($notApi) {
+        return $idLineaProductoPerGuia->toArray();
+      }
+
+      return response()->json(["status" => 1, "message" => $idLineaProductoPerGuia]);
+    } catch (\Throwable $th) {
+      return response()->json(["Error" => $th->getMessage(), "Línea" => $th->getLine(), "Archivo" => __FILE__]);
+    }
+  }
+
+  public function getDocumentoPDF($id_guia, $id_documento)
+  {
+    try {
+
+
+      $documento = $this->getDocumentosPerGuia($id_guia, true);
+
+      $documento = array_filter($documento, function ($doc) use ($id_documento) {
+        return $doc["id_documento_entrega"] == $id_documento;
+      })[0];
+
+      $data = [
+        // "id_documento_entrega" => $documento["id_documento_entrega"],
+        "total" => +$documento["precio"] * +$documento["cantidad"],
+        // "direccion_entrega" => $documento["direccion_entrega"],
+        "dias_fabricacion" => $documento["dias_fabricacion"],
+        // "fecha_despacho" => $documento["fecha_despacho"],
+        // "fecha_entrega" => $documento["fecha_entrega"],
+        // "tipo_producto" => $documento["tipo_producto"],
+        "descripcion" => $documento["descripcion"],
+        "id_pedido" => $documento["id_pedido"],
+        "cantidad" => $documento["cantidad"],
+        // "estado" => $documento["estado"],
+        "precio" => $documento["precio"],
+        "SKU" => $documento["SKU"],
+        // "id" => $documento["id"],
+      ];
+
+      return PDF::loadView('documento', $data)->stream("Documento {$documento["id_documento_entrega"]}");
     } catch (\Throwable $th) {
       return response()->json(["Error" => $th->getMessage(), "Línea" => $th->getLine(), "Archivo" => __FILE__]);
     }
