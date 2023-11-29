309f54610d3ed47007af7afc699e0e6d9067cfc3
Jose Arroyave <poer0601@hotmail.com>
2023-11-29 17:21:19 -0500

    terminar lógica de guias

--- app/Http/Controllers/FpGuiasEnvioController.php
+++ app/Http/Controllers/FpGuiasEnvioController.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use App\Models\FpProductosPorPedidosModel;
 use App\Models\FpDocumentosEntregasModel;
+use Barryvdh\DomPDF\Facade\Pdf as PDF;
 use Illuminate\Routing\Controller;
 use App\Models\FpGuiasEnvioModel;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class FpGuiasEnvioController extends Controller
 {
@@ -46,7 +45,7 @@ class FpGuiasEnvioController extends Controller
     }
   }
 
-  public function getGuiasPerClient(Request $request)
+  public function getGuiasPerClient($id_cliente, $notApi = false)
   {
     try {
 
@@ -54,14 +53,57 @@ class FpGuiasEnvioController extends Controller
         ->join("fp_pedidos", "fp_productos_x_pedido.id_pedido", "fp_pedidos.id_pedido")
         ->join("fp_productos", "fp_productos_x_pedido.SKU", "fp_productos.SKU")
         ->join("fp_clientes", "fp_pedidos.id_cliente", "fp_clientes.id_cliente")
-        ->where([
-          ["fp_pedidos.id_cliente", $request->id_cliente],
-          ["fp_clientes.id_cliente", $request->id_cliente],
+        ->select([
+          "fp_guias.id_guia",
+          "fp_clientes.nombre",
+          "fp_clientes.apellido",
+          "fp_clientes.id_cliente",
+          "fp_productos_x_pedido.fecha_entrega",
+          "fp_productos_x_pedido.fecha_despacho",
+          "fp_productos_x_pedido.direccion_entrega",
         ])
+        ->where([
+          ["fp_pedidos.id_cliente", $id_cliente],
+          ["fp_clientes.id_cliente", $id_cliente],
+        ])
+        ->groupBy('fp_guias.id_guia', 'fp_clientes.nombre', 'fp_clientes.apellido', 'fp_clientes.id_cliente', 'fp_productos_x_pedido.fecha_entrega', 'fp_productos_x_pedido.fecha_despacho', 'fp_productos_x_pedido.direccion_entrega')
         ->get()
         ->toArray();
 
+      if ($notApi) {
+        return $guias;
+      }
+
       return response()->json(["status" => 1, "message" => $guias]);
+    } catch (\Throwable $th) {
+      return response()->json(["Error" => $th->getMessage(), "Línea" => $th->getLine(), "Archivo" => __FILE__]);
+    }
+  }
+
+  public function getGuiaPDF($id_guia, $id_cliente)
+  {
+    try {
+
+      $guia = $this->getGuiasPerClient($id_cliente, true);
+
+      $guia = array_values(array_filter($guia, function ($gui) use ($id_guia) {
+        return $gui["id_guia"] == $id_guia;
+      }))[0];
+
+      $qrcode = base64_encode(QrCode::generate("http://192.168.2.103:4000/POD/$id_guia}"));
+
+      $data = [
+        "direccion_entrega" => $guia["direccion_entrega"],
+        "fecha_despacho" => $guia["fecha_despacho"],
+        "fecha_entrega" => $guia["fecha_entrega"],
+        "id_cliente" => $guia["id_cliente"],
+        "apellido" => $guia["apellido"],
+        "id_guia" => $guia["id_guia"],
+        "nombre" => $guia["nombre"],
+        "qr" => $qrcode
+      ];
+
+      return PDF::loadView('guia', $data)->stream("Guia {$guia["id_guia"]}");
     } catch (\Throwable $th) {
       return response()->json(["Error" => $th->getMessage(), "Línea" => $th->getLine(), "Archivo" => __FILE__]);
     }
